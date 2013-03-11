dda2566e5f40e7fa6699e209061fdb49a9467413
Toon Schoenmakers <nighteyes1993@gmail.com>
2013-03-12 00:39:20 +0100

    Probably smart to default this to 0 as it's the only valid option at the moment

--- database_schema.sql
+++ database_schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE allowed_in_mail (
 CREATE TABLE push_ids (
     email character varying(64) NOT NULL,
     push_id character varying(256) NOT NULL,
-    push_type integer
+    push_type integer DEFAULT 0 NOT NULL
 );
 ALTER TABLE ONLY allowed_in_mail
     ADD CONSTRAINT allowed_in_mail_pkey PRIMARY KEY (email);
